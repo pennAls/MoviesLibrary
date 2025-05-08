16fd0cffa08b2dccfad1a1419e7ff4e90ed7453b
pennA <lucaspsantos003@gmail.com>
2025-05-08 12:35:10 -0300

    Fix - Debounce searchInput

--- movies_library/src/components/SearchInput/SearchInput.tsx
+++ movies_library/src/components/SearchInput/SearchInput.tsx
@@ -1,36 +1,50 @@
 import { moviesType } from "../../types/moviesType.ts";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { getMovies } from "../../services/moviesService.ts";
 import { searchedMoviesListContext } from "../../App/App.tsx";
 
 export const SearchInput = ({ style }: CSSModuleClasses) => {
   const { setsearchedMovies } = useContext(searchedMoviesListContext);
+  const [inputValue, setInputValue] = useState("");
+  const [timeoutId, setTimeoutId] = useState<number | null>(null);
 
   useEffect(() => {
     setsearchedMovies([]);
   }, [setsearchedMovies]);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pesquisaUser = e.target.value;
+    setInputValue(pesquisaUser);
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    const newTimeoutId = setTimeout(async () => {
+      if (pesquisaUser === "") {
+        setsearchedMovies([]);
+      } else {
+        const searchedMoviesList = await getMovies();
+        const resultadoPesquisa = searchedMoviesList.filter(
+          (movie: moviesType) =>
+            movie.nome.toLowerCase().includes(pesquisaUser.trim().toLowerCase())
+        );
+        setsearchedMovies(resultadoPesquisa);
+        sessionStorage.setItem("userInput", pesquisaUser);
+      }
+    }, 500);
+
+    setTimeoutId(newTimeoutId);
+  };
+
   return (
     <>
       <input
         className={style}
-        onChange={async (e) => {
-          const pesquisaUser = e.target.value;
-          if (pesquisaUser === "") {
-            setsearchedMovies([]);
-          } else {
-            const searchedMoviesList = await getMovies();
-            const resultadoPesquisa = searchedMoviesList.filter(
-              (movie: moviesType) =>
-                movie.nome.toLowerCase().includes(pesquisaUser.toLowerCase())
-            );
-            setsearchedMovies(resultadoPesquisa);
-            sessionStorage.setItem("userInput", pesquisaUser);
-          }
-        }}
+        onChange={handleChange}
         placeholder="Qual o seu próximo filme?"
         type="text"
+        value={inputValue}
       />
     </>
   );
